8b836145846cf08edb99315ee9806f7fd225dd48
Theresa Abutu <theresa.abutu>
2023-03-26 21:34:42 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const name = "Theresa Abutu";
+const language = "JavaScript";
+const future = "In the next year, I see myself becoming more proficient in " + language + " and becoming a very good backend engineer.";
+
+console.log("My name is " + name + ".");
+console.log("My preferred programming language is " + language + ".");
+console.log(future);
